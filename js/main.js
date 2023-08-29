2326825ff56ffc004fe021018b27a905d4c68c27
markok1 <markogit123@gmail.com>
2023-08-29 16:43:06 +0200

    added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 (function ($) {
   let section = $("section.faq");
+  let sectionForm = $("div.form-holder");
   if (section.length) {
     $(".qa-item").click(function (e) {
       e.preventDefault();
@@ -11,6 +12,42 @@
         $(".qa-item .answer").slideUp(150);
         $(this).addClass("opened");
         $(this).find(".answer").slideDown(150);
+      }
+    });
+  }
+  if (sectionForm.length) {
+    $(".form-dropdown-label").click(function (e) {
+      e.preventDefault();
+      if ($(this).parent().hasClass("quote-form-active")) {
+        $(this).parent().removeClass("quote-form-active");
+      } else {
+        $(this).parent().addClass("quote-form-active");
+      }
+    });
+    $(
+      ".form-dropdown-container .dropdown-list-custom .dropdown-list-item"
+    ).click(function (e) {
+      e.preventDefault();
+      var val = $(this).find("p").text();
+      $(this)
+        .parent()
+        .parent()
+        .parent()
+        .parent()
+        .parent()
+        .find(".option-text")
+        .text(val);
+      $(this)
+        .parent()
+        .parent()
+        .find(".dropdown-list-item--active")
+        .removeClass("dropdown-list-item--active");
+      $(this).addClass("dropdown-list-item--active");
+      $(".quote-form-active").removeClass("quote-form-active");
+    });
+    $(document).click(function (event) {
+      if (!$(event.target).closest(".form-dropdown-container").length) {
+        $(".quote-form-active").removeClass("quote-form-active");
       }
     });
   }
